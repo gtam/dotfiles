e35a84d17d2013c88a24a729dc9109d2aaa595df
gtam <gtam@users.noreply.github.com>
2019-09-17 16:20:46 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ TMUX_FILES_DIR=~/.tmux.git
     echo "${TMUX_FILES_DIR} already exists.  Existing."
     exit 1
 }
-git clone git clone https://github.com/gpakosz/.tmux.git ~/.
+git clone https://github.com/gpakosz/.tmux.git ~/.
 ln -s -f ~/.tmux/.tmux.conf ~/.
 
 git clone https://github.com/gtam/dotfiles.git ${DOT_FILES_DIR} || {
