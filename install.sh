b6870ee9dd9aa914ce68d53eb3a9be3a73fe36a0
gtam <gtam@users.noreply.github.com>
2019-09-17 16:24:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 DOT_FILES_DIR=~/.dotfiles
-TMUX_FILES_DIR=~/.tmux.git
+TMUX_FILES_DIR=~/.tmux
 
 [ -e ${DOT_FILES_DIR} ] && {
-    echo "${DOT_FILES_DIR} already exists.  Existing."
+    echo "${DOT_FILES_DIR} already exists.  Exiting."
     exit 1
 }
 
 [ -e ${TMUX_FILES_DIR} ] && {
-    echo "${TMUX_FILES_DIR} already exists.  Existing."
+    echo "${TMUX_FILES_DIR} already exists.  Exiting."
     exit 1
 }
-git clone https://github.com/gpakosz/.tmux.git ~/.
+git clone https://github.com/gpakosz/.tmux.git ${TMUX_FILES_DIR} || {
+    echo "Git clone failed. Exiting"
+    exit 1
 ln -s -f ~/.tmux/.tmux.conf ~/.
 
 git clone https://github.com/gtam/dotfiles.git ${DOT_FILES_DIR} || {
-    echo 'Git clone failed.  Exiting.'
+    echo "Git clone failed.  Exiting."
     exit 1
 }
 
