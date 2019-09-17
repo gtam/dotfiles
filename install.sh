5ed79975d862f97e312be538a6612dd85041178d
Garrick Tam <gtam@Garricks-MacBook-Pro.local>
2019-09-17 16:12:28 -0700

    Incorporated https://github.com/gpakosz/.tmux with my own customized .tmux.conf.local changes.

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 DOT_FILES_DIR=~/.dotfiles
+TMUX_FILES_DIR=~/.tmux.git
 
 [ -e ${DOT_FILES_DIR} ] && {
     echo "${DOT_FILES_DIR} already exists.  Existing."
     exit 1
 }
+
+[ -e ${TMUX_FILES_DIR} ] && {
+    echo "${TMUX_FILES_DIR} already exists.  Existing."
+    exit 1
+}
+git clone git clone https://github.com/gpakosz/.tmux.git ~/.
+ln -s -f ~/.tmux/.tmux.conf ~/.
 
 git clone https://github.com/gtam/dotfiles.git ${DOT_FILES_DIR} || {
     echo 'Git clone failed.  Exiting.'
