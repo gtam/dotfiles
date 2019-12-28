9ac29015322f5a566962a5fef5f99f61d600099c
Garrick Tam <garrick.tam@gmail.com>
2019-12-28 12:45:31 -0800

    Fix reading decision from subshell

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ for i in git make
 
 [ -e ${DOT_FILES_DIR} ] && {
     echo "${DOT_FILES_DIR} already exists.  Delete ${DOT_FILES_DIR} and continue? "
-    read -p '(Y/n): ' decision1
+    read -p '(Y/n): ' decision1 < /dev/tty
     if [[ "$decision1" == "Y" ]]; then
       rm -rf ${DOT_FILES_DIR}
     else
@@ -28,7 +28,7 @@ for i in git make
 
 [ -e ${TMUX_FILES_DIR} ] && {
     echo "${TMUX_FILES_DIR} already exists.  Delete ${TMUX_FILES_DIR} and continue? "
-    read -p '(Y/n): ' decision2
+    read -p '(Y/n): ' decision2 < /dev/tty
     if [[ "$decision2" == "Y" ]]; then
       rm -rf ${TMUX_FILES_DIR}
     else
