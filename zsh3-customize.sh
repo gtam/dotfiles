a5c59100f136f0a7ab05fb88717312523e68641e
gtam <gtam@users.noreply.github.com>
2019-12-29 10:47:48 -0800

    Update zsh3-customize.sh

--- zsh3-customize.sh
+++ zsh3-customize.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 [ -e ${HOME}/.dotfiles/robbyrussell.zsh-theme ] && {
-  echo "Updating robbyrussel.zsh-theme with customizations."
-  cp ${HOME}/.dotfiles/robbyrussell.zsh-theme ${HOME}/.oh-my-zsh/themes/robbyrussell.zsh-theme
+  echo "Symbolic linking robbyrussel.zsh-theme with customizations."
+  ln -sfb ${HOME}/.dotfiles/robbyrussell.zsh-theme ${HOME}/.oh-my-zsh/themes/robbyrussell.zsh-theme
   }
 source ${HOME}/.zshrc
