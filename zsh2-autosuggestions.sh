dacf018c29d873503ca2b5ba31cd88ab640b4237
gtam <gtam@users.noreply.github.com>
2020-02-06 20:31:23 -0800

    Update zsh2-autosuggestions.sh

--- zsh2-autosuggestions.sh
+++ zsh2-autosuggestions.sh
@@ -5,7 +5,7 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     retval=$?
     if [ $retval = 0 ]; then
-    sed -i "/plugins=/ s/^\(.*\)\()\)/\1 zsh-autosuggestions\2/" $HOME/.zshrc
+    sed -i.bak "/plugins=/ s/^\(.*\)\()\)/\1 zsh-autosuggestions\2/" $HOME/.zshrc
     fi
 else
     echo "oh-my-zsh not install."
