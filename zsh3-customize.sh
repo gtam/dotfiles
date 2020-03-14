47c75414c5c9afbb6bbaceb1083098f8c8ffd643
Garrick Tam <gtam@Garricks-MacBook-Pro.local>
2020-03-13 22:00:02 -0700

    cleanup

--- zsh3-customize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-[ -e ${HOME}/.dotfiles/robbyrussell.zsh-theme ] && {
-  echo "Symbolic linking robbyrussel.zsh-theme with customizations."
-  ln -sfb ${HOME}/.dotfiles/robbyrussell.zsh-theme ${HOME}/.oh-my-zsh/themes/robbyrussell.zsh-theme
-  }
-source ${HOME}/.zshrc
